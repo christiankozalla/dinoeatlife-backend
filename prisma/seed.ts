e188516c031d1e80a192cecf0b4f73045d475573
christiankozalla <christian.kozalla@gmail.com>
2021-05-30 17:41:57 +0200

    New prisma schema without Token model - with model Profile public - model User private

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,32 +3,32 @@ const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient();
 
-const seedData = [
-  {
-    name: "Christian",
-    email: "christian@christian.de",
-    password: "christiansNicePassword",
-    household: {
-      create: { name: "Koala-Bude" },
-    },
-  },
-  {
-    name: "Bingo",
-    email: "bingo@lino.de",
-    password: "bingosNicePassword",
-    household: {
-      create: { name: "BingoBingo" },
-    },
-  },
-  {
-    name: "Lalala",
-    email: "lalala@lo.de",
-    password: "lalalasNicePassword",
-    household: {
-      create: { name: "Alalalalalong" },
-    },
-  },
-];
+// const seedData = [
+//   {
+//     name: "Christian",
+//     email: "christian@christian.de",
+//     password: "christiansNicePassword",
+//     household: {
+//       create: { name: "Koala-Bude" },
+//     },
+//   },
+//   {
+//     name: "Bingo",
+//     email: "bingo@lino.de",
+//     password: "bingosNicePassword",
+//     household: {
+//       create: { name: "BingoBingo" },
+//     },
+//   },
+//   {
+//     name: "Lalala",
+//     email: "lalala@lo.de",
+//     password: "lalalasNicePassword",
+//     household: {
+//       create: { name: "Alalalalalong" },
+//     },
+//   },
+// ];
 
 // Add second user to household
 // const secondUser = {
@@ -37,12 +37,46 @@ const seedData = [
 //   password: "stephiesNicePassword",
 // };
 
+const userProfiles = [
+  {
+    userId: 1,
+    name: "Christian",
+    city: "Dresden",
+    bio: "My live is a blast.....",
+    social: {
+      twitter: "@CKozalla",
+      github: "christiankozalla",
+      website: "chrisko.io"
+    }
+  },
+  {
+    userId: 2,
+    name: "Dirk",
+    city: "Dortmund",
+    bio: "Haaaaaleeelujaaaah",
+    social: {
+      twitter: "@dirkydirk",
+      github: "rollo"
+    }
+  },
+  {
+    userId: 4,
+    name: "Stephanie",
+    city: "Dresden",
+    bio: "Yipppie",
+    social: {
+      twitter: "@stephieswerk",
+      website: "www.stephieswerk.de"
+    }
+  }
+];
+
 async function main() {
   console.log(`Start seeding ...`);
-  for (let u of seedData) {
-    u.password = await bcrypt.hash(u.password, 10);
-    const user = await prisma.user.create({
-      data: u,
+  for (let u of userProfiles) {
+    // u.password = await bcrypt.hash(u.password, 10);
+    const user = await prisma.profile.create({
+      data: u
     });
     console.log(`Created user with id: ${user.id}`);
   }
