e6c4e93c4c8ec3277d806a683824dba4257801f1
christiankozalla <christian.kozalla@gmail.com>
2021-06-05 08:51:21 +0200

    Home endpoint GET method set up

--- src/plugins/home.ts
+++ src/plugins/home.ts
@@ -1,6 +1,7 @@
 import Hapi from "@hapi/hapi";
 import { Ingredient, Recipe, PrismaClient } from "@prisma/client";
 import Boom from "@hapi/boom";
+import { userIsAuthorized } from "../util/authorization";
 import Joi from "joi";
 
 declare module "@hapi/hapi" {
@@ -27,52 +28,39 @@ export const homePlugin: Hapi.Plugin<null> = {
         handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
           // Returns all resources of a Home for an authenticated User of the Home
           // Resources: Recipe[], Ingredient[]
-          
+
           const { prisma } = request.server.app;
+          const homeId = parseInt(request.params.homeId, 10);
           const { credentials } = request.auth;
 
-          try {
-            // Fetch the home users
-            const allUserIds = await prisma.home.findUnique({
-              where: {
-                id: credentials.homeId as credentials["homeId"]
-              },
-              select: {
-                users: {
-                  select: {
-                    id: true
+          if (homeId === credentials.homeId) {
+            try {
+              if (userIsAuthorized(homeId, credentials.userId as credentials["userId"], prisma)) {
+                // The user truely belongs to the home
+
+                // Fetch all recipes
+                const recipes: Recipe[] = await prisma.recipe.findMany({
+                  where: {
+                    homeId: credentials.homeId as credentials["homeId"]
                   }
-                }
+                });
+
+                // Fetch all ingredients
+                const ingredients: Ingredient[] = await prisma.ingredient.findMany({
+                  where: {
+                    homeId: credentials.homeId as credentials["homeId"]
+                  }
+                });
+
+                return h.response({ recipes, ingredients }).code(200);
+              } else {
+                return Boom.unauthorized();
               }
-            });
-
-            if (!allUserIds) {
-              return Boom.notFound("No users in home");
+            } catch (err) {
+              return Boom.badImplementation(err);
             }
-
-            if (allUserIds.users.some((data) => data.id === credentials.userId)) {
-              // The user truely belongs to the home
-
-              // Fetch all recipes
-              const recipes: Recipe[] = await prisma.recipe.findMany({
-                where: {
-                  homeId: credentials.homeId as credentials["homeId"]
-                }
-              });
-
-              // Fetch all ingredients
-              const ingredients: Ingredient[] = await prisma.ingredient.findMany({
-                where: {
-                  homeId: credentials.homeId as credentials["homeId"]
-                }
-              });
-
-              return h.response({ recipes, ingredients }).code(200);
-            } else {
-              return Boom.forbidden("You cannot access that data.");
-            }
-          } catch (err) {
-            return Boom.badImplementation(err);
+          } else {
+            return Boom.unauthorized();
           }
         }
       }
