ae730eae1b5759b4e855fe04c143f94dc9230159
christiankozalla <christian.kozalla@gmail.com>
2021-06-08 09:01:37 +0200

    Updates seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,128 +3,128 @@ const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient();
 
-// const seedData = [
-//   {
-//     email: "christian@christian.de",
-//     password: "christiansNicePassword",
-//     home: {
-//       create: { name: "Koala-Bude" }
-//     }
-//   },
-//   {
-//     email: "bingo@lino.de",
-//     password: "bingosNicePassword",
-//     home: {
-//       create: { name: "BingoBingo" }
-//     }
-//   },
-//   {
-//     email: "lalala@lo.de",
-//     password: "lalalasNicePassword",
-//     home: {
-//       create: { name: "Alalalalalong" }
-//     }
-//   }
-// ];
+const seedData = [
+  {
+    email: "christian@christian.de",
+    password: "christiansNicePassword",
+    home: {
+      create: { name: "Koala-Bude" }
+    }
+  },
+  {
+    email: "bingo@lino.de",
+    password: "bingosNicePassword",
+    home: {
+      create: { name: "BingoBingo" }
+    }
+  },
+  {
+    email: "lalala@lo.de",
+    password: "lalalasNicePassword",
+    home: {
+      create: { name: "Alalalalalong" }
+    }
+  }
+];
 
-// //Add second user to home
-// const secondUser = {
-//   email: "stephie@stephie.de",
-//   password: "stephiesNicePassword"
-// };
+//Add second user to home
+const secondUser = {
+  email: "stephie@stephie.de",
+  password: "stephiesNicePassword"
+};
 
-// const userProfiles = [
-//   {
-//     name: "Christian",
-//     city: "Dresden",
-//     bio: "My live is a blast.....",
-//     social: {
-//       twitter: "@CKozalla",
-//       github: "christiankozalla",
-//       website: "chrisko.io"
-//     }
-//   },
-//   {
-//     name: "Dirk",
-//     city: "Dortmund",
-//     bio: "Haaaaaleeelujaaaah",
-//     social: {
-//       twitter: "@dirkydirk",
-//       github: "rollo"
-//     }
-//   },
-//   {
-//     name: "Stephanie",
-//     city: "Dresden",
-//     bio: "Yipppie",
-//     social: {
-//       twitter: "@stephieswerk",
-//       website: "www.stephieswerk.de"
-//     }
-//   }
-// ];
-
-// async function main() {
-//   console.log(`Start seeding ...`);
-//   for (let i = 0; i < seedData.length; i++) {
-//     seedData[i].password = await bcrypt.hash(seedData[i].password, 10);
-//     const user = await prisma.user.create({
-//       data: seedData[i]
-//     });
-
-//     await prisma.profile.create({
-//       data: {
-//         ...userProfiles[i],
-//         user: {
-//           connect: { email: seedData[i].email }
-//         }
-//       }
-//     });
-//     console.log(`Created user with id: ${user.id}`);
-//   }
-
-//   secondUser.password = await bcrypt.hash(secondUser.password, 10);
-
-//   await prisma.user.create({
-//     data: {
-//       ...secondUser,
-//       homeId: 1,
-//     },
-//   });
-//   console.log(`Seeding finished.`);
-// }
+const userProfiles = [
+  {
+    name: "Christian",
+    city: "Dresden",
+    bio: "My live is a blast.....",
+    social: {
+      twitter: "@CKozalla",
+      github: "christiankozalla",
+      website: "chrisko.io"
+    }
+  },
+  {
+    name: "Dirk",
+    city: "Dortmund",
+    bio: "Haaaaaleeelujaaaah",
+    social: {
+      twitter: "@dirkydirk",
+      github: "rollo"
+    }
+  },
+  {
+    name: "Stephanie",
+    city: "Dresden",
+    bio: "Yipppie",
+    social: {
+      twitter: "@stephieswerk",
+      website: "www.stephieswerk.de"
+    }
+  }
+];
 
 async function main() {
   console.log(`Start seeding ...`);
+  for (let i = 0; i < seedData.length; i++) {
+    seedData[i].password = await bcrypt.hash(seedData[i].password, 10);
+    const user = await prisma.user.create({
+      data: seedData[i]
+    });
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: "christian@christian.de"
-    }
-  });
-
-  console.log(user);
-
-  await prisma.recipe.create({
-    data: {
-      name: "New Recipe",
-      duration: 1000,
-      description: "A new recipe created by christian",
-      user: {
-        connect: {
-          id: user.id
-        }
-      },
-      home: {
-        connect: {
-          id: user.homeId
+    await prisma.profile.create({
+      data: {
+        ...userProfiles[i],
+        user: {
+          connect: { email: seedData[i].email }
         }
       }
-    }
-  });
+    });
+    console.log(`Created user with id: ${user.id}`);
+  }
 
+  secondUser.password = await bcrypt.hash(secondUser.password, 10);
+
+  await prisma.user.create({
+    data: {
+      ...secondUser,
+      homeId: 1,
+    },
+  });
   console.log(`Seeding finished.`);
 }
+
+// async function main() {
+//   console.log(`Start seeding ...`);
+
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       email: "christian@christian.de"
+//     }
+//   });
+
+//   console.log(user);
+
+//   await prisma.recipe.create({
+//     data: {
+//       name: "New Recipe",
+//       duration: 1000,
+//       description: "A new recipe created by christian",
+//       user: {
+//         connect: {
+//           id: user.id
+//         }
+//       },
+//       home: {
+//         connect: {
+//           id: user.homeId
+//         }
+//       }
+//     }
+//   });
+
+//   console.log(`Seeding finished.`);
+// }
 
 main()
   .catch((e) => {
